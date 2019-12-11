08fbd95ab11b98836d8e11275375e5e1e7bf6b88
miguel.salamero <miguel.salamero@est.fib.upc.edu>
2019-12-11 23:13:37 +0100

    add new issue html

--- src/components/CreateIssue.vue
+++ src/components/CreateIssue.vue
@@ -1,10 +1,94 @@
 <template>
-  
-  <div class="CreateIssue">
-  <h1>Create issue</h1>
-  </div>
+  <div >
+    <div class="header pt-5 text-left">
+      <h1>New issue</h1>
+    </div>
+    <br>
+    <div class="px-5 text-left">
+  <form>
 
+    <label for="issue_Title">Title</label>
+    <div class="form-group">
+      <div class="field">
+        <input type="text" name="issue[Title]" id="issue_Title">
+      </div>
+    </div>
+
+    <label for="issue_Description">Description</label>
+    <div class="form-group">
+      <div class="field"> 
+      <textarea name="issue[Description]" id="issue_Description" cols="100" rows="5"></textarea>
+      </div>
+
+    </div>
+        
+    <label for="issue_Type">Type</label>
+    <div class="form-group">
+      <div class="field">
+        <select name="issue[Type]" id="issue_Type"><option value="Bug" selected="selected">Bug</option>
+<option value="Enhancement">Enhancement</option>
+<option value="Proposal">Proposal</option>
+<option value="Task">Task</option></select>
+      </div>
+    </div>
+
+    <label for="issue_Priority">Priority</label>
+    <div class="field">
+      <select name="issue[Priority]" id="issue_Priority"><option value="Trivial" selected="selected">Trivial</option>
+<option value="Minor">Minor</option>
+<option value="Major">Major</option>
+<option value="Critical">Critical</option>
+<option value="Blocker">Blocker</option></select>
+    </div>
+    
+    <label for="issue_Assignee">Assignee</label>
+    <div class="input-group">
+      <select name="issue[asignee_id]" id="issue_asignee_id"><option value="" selected="selected">No assigned user</option>
+<option value="1">Miguel</option>
+<option value="2">Cristian R.</option>
+<option value="3">Carles Farré</option>
+<option value="4">sisu psicologo</option>
+<option value="5">Jaume Malgosa Broto</option>
+<option value="6">Marc obiols rubio</option></select>
+    </div>
+
+    <br>
+
+    <div class="field" style="display:none;">
+      <input required="required" class="form-control" value="1" type="text" name="issue[user_id]" id="issue_user_id">
+      <input required="required" class="form-control" value="New" type="text" name="issue[Status]" id="issue_Status">
+    </div>
+    
+    <div class="form-group">
+      <label for="issue_Attach">Attach</label>
+      <input type="file" name="issue[attachment]" id="issue_attachment">
+    </div>
+
+    <div style="float:right">
+      <input type="submit" name="commit" value="Create Issue" class="btn btn-outline-dark" data-disable-with="Create Issue">
+    <button type="button" class="btn btn-outline-dark"><router-link to="/">Back</router-link>
+      
+    </button>
+    </div>
+</form>
+  <form>
+
+  </form>
+</div>
+  </div>
 </template>
+
+
+
+
+
+
+
+
+
+
+
+
 
 <script>
 
