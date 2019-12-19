6a47e403ae1a7d0db661e148f8bdb48b7f895f15
miguel.salamero <miguel.salamero@est.fib.upc.edu>
2019-12-19 08:30:13 +0100

    delete unuseful views

--- src/router/index.js
+++ src/router/index.js
@@ -15,14 +15,6 @@ const routes = [
     component: Issues
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/newissue',
     name: 'CreateIssue',
     component: CreateIssue
